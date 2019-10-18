c124edb2cfd5587f20a65f54cee55741bec24fa4
Mike Schilling <msbindstone@gmail.com>
2019-10-18 18:47:33 +0200

    Init Project

--- ui/src/components/client/client-modify.js
+++ ui/src/components/client/client-modify.js
@@ -1,97 +1,106 @@
-import React from 'react';
-import {withRouter} from "react-router";
-import {graphql, Query} from "react-apollo";
-import {getClient, updateUserWithCurrency} from "../../queries/client-query";
+import React from "react";
+import { withRouter } from "react-router";
+import { graphql, Query } from "react-apollo";
+import { getClient, updateUserWithCurrency } from "../../queries/client-query";
 import Form from "react-bootstrap/Form";
 import CurrencyCombobox from "../currency/currency-combobox";
-import {Button} from "react-bootstrap";
-
+import { Button } from "react-bootstrap";
 
 class ClientModify extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            clientId: "",
-            firstName: "",
-            lastName: "",
-            currencyId: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: "",
+      firstName: "",
+      lastName: "",
+      currencyId: ""
     };
+  }
 
-    render() {
-        const id = this.props.match.params.id;
-        return (
-            <Query query={getClient} variables={{id}} onCompleted={this.updateStates}>
-                {({loading, error, data}) => {
-                    if (loading) return <p>Loading...</p>;
+  save = e => {
+    e.preventDefault();
 
-                    if (error) return <p>Error</p>;
+    this.props
+      .mutate({
+        variables: {
+          clientId: this.state.clientId,
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          currencyId: this.state.currencyId
+        },
+        refetchQueries: [{ query: getClient }]
+      })
+      .then(e => {
+        this.props.history.push("/client");
+      })
+      .catch(e => console.log(e));
+  };
 
-                    return this.renderForm(data);
-                }}
-            </Query>
-        );
-    }
+  render() {
+    const id = this.props.match.params.id;
+    return (
+      <Query
+        query={getClient}
+        variables={{ id }}
+        onCompleted={this.updateStates}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
 
-    updateStates = (data) => {
-        const client = data.Client[0];
-        this.setState({firstName: client.firstName});
-        this.setState({lastName: client.lastName});
-        this.setState({clientId: client.id});
-        this.setState({currencyId: client.currency.id});
-    };
+          if (error) return <p>Error</p>;
 
-    save = (e) => {
-        e.preventDefault();
-        const variables = {
-            clientId: this.state.clientId,
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            currencyId: this.state.currencyId
-        };
-        console.log(variables);
+          return this.renderForm(data);
+        }}
+      </Query>
+    );
+  }
 
-        this.props.mutate({
-            variables: variables,
-            refetchQueries: ['GetClients', 'GetClient']
-        }).then(e => {
-            this.props.history.push('/client');
-        }).catch(e => console.log(e));
-    };
+  updateStates = data => {
+    const client = data.Client[0];
+    this.setState({ firstName: client.firstName });
+    this.setState({ lastName: client.lastName });
+    this.setState({ clientId: client.id });
+    this.setState({ currencyId: client.currency.id });
+  };
 
-    renderForm() {
-        return (
-            <Form onSubmit={e => this.save(e)}>
+  renderForm() {
+    return (
+      <Form onSubmit={e => this.save(e)}>
+        <Form.Group>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            placeholder="First Name"
+            type="text"
+            onChange={e => this.setState({ firstName: e.target.value })}
+            value={this.state.firstName}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control placeholder="First Name" type="text"
-                                  onChange={e => this.setState({firstName: e.target.value})}
-                                  value={this.state.firstName}
-                    />
-                </Form.Group>
+        <Form.Group>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            placeholder="Last Name"
+            type="text"
+            onChange={e => this.setState({ lastName: e.target.value })}
+            value={this.state.lastName}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control placeholder="Last Name" type="text"
-                                  onChange={e => this.setState({lastName: e.target.value})}
-                                  value={this.state.lastName}
-                    />
-                </Form.Group>
+        <Form.Group>
+          <Form.Label>Currency</Form.Label>
+          <CurrencyCombobox
+            className="form-control"
+            onChange={e => this.setState({ currencyId: e.target.value })}
+            value={this.state.currencyId}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Currency</Form.Label>
-                    <CurrencyCombobox className="form-control"
-                                      onChange={e => this.setState({currencyId: e.target.value})}
-                                      value={this.state.currencyId}
-                    />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">Save</Button>
-            </Form>
-        )
-    }
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    );
+  }
 }
 
-export default (graphql(updateUserWithCurrency)(withRouter(ClientModify)));
+export default graphql(updateUserWithCurrency)(withRouter(ClientModify));
--- ui/src/components/client/client-new.js
+++ ui/src/components/client/client-new.js
@@ -1,69 +1,77 @@
-import React from 'react';
-import {addClientCurrency} from "../../queries/client-query";
-import {graphql} from "react-apollo";
+import React from "react";
+import { addClientCurrency, getClients } from "../../queries/client-query";
+import { graphql } from "react-apollo";
 import CurrencyCombobox from "../currency/currency-combobox";
-import {withRouter} from 'react-router-dom'
-import {Button} from "react-bootstrap"
+import { withRouter } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 class ClientNew extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            currencyId: ''
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      currencyId: ""
     };
+  }
 
-    save = (e) => {
-        e.preventDefault();
+  save = e => {
+    e.preventDefault();
 
-        this.props.mutate({
-            variables: {
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                currencyId: this.state.currencyId
-            },
-            refetchQueries: ['GetClients']
-        }).then(e => {
-            this.props.history.push('/client');
-        }).catch(e => console.log(e));
-    };
+    this.props
+      .mutate({
+        variables: {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          currencyId: this.state.currencyId
+        },
+        refetchQueries: [{ query: getClients }]
+      })
+      .then(e => {
+        this.props.history.push("/client");
+      })
+      .catch(e => console.log(e));
+  };
 
-    render() {
-        return (
-            <Form onSubmit={e => this.save(e)}>
+  render() {
+    return (
+      <Form onSubmit={e => this.save(e)}>
+        <Form.Group>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            placeholder="First Name"
+            type="text"
+            onChange={e => this.setState({ firstName: e.target.value })}
+            value={this.state.firstName}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control placeholder="First Name" type="text"
-                                  onChange={e => this.setState({firstName: e.target.value})}
-                                  value={this.state.firstName}
-                    />
-                </Form.Group>
+        <Form.Group>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            placeholder="Last Name"
+            type="text"
+            onChange={e => this.setState({ lastName: e.target.value })}
+            value={this.state.lastName}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control placeholder="Last Name" type="text"
-                                  onChange={e => this.setState({lastName: e.target.value})}
-                                  value={this.state.lastName}
-                    />
-                </Form.Group>
+        <Form.Group>
+          <Form.Label>Currency</Form.Label>
+          <CurrencyCombobox
+            className="form-control"
+            onChange={e => this.setState({ currencyId: e.target.value })}
+            value={this.state.currencyId}
+          />
+        </Form.Group>
 
-                <Form.Group>
-                    <Form.Label>Currency</Form.Label>
-                    <CurrencyCombobox className="form-control"
-                                      onChange={e => this.setState({currencyId: e.target.value})}
-                                      value={this.state.currencyId}
-                    />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">Save</Button>
-            </Form>
-        )
-    }
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    );
+  }
 }
 
-export default (graphql(addClientCurrency)(withRouter(ClientNew)));
+export default graphql(addClientCurrency)(withRouter(ClientNew));
